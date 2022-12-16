9d4b2c9f37e321abfc307883a5155b50d6ef882e
Gleidson <gleidsonnq@gmail.com>
2022-12-16 11:10:51 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using System.IO.Ports;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
